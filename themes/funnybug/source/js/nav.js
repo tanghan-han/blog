563ad2cc70a15a1e1899916f027afa8a4f996ec1
tnaghan <user2@163.com>
2020-06-29 20:34:30 +0800

    调整导航栏

--- themes/funnybug/source/js/nav.js
+++
@@ -1,9 +0,0 @@
-// 监听事件
-window.addEventListener('scroll', function(){
- 	let t = $('body, html').scrollTop();// 目前监听的是整个body的滚动条距离
- 	if(t>0){
-		$('nav').addClass('nav-active')
-	}else{
-		$('nav').removeClass('nav-active')
-	}
-})
