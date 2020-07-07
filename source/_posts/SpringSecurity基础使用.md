179d24fb10a0fbb38f9cec04167ffeef0ea35a1f
tnaghan <user2@163.com>
2020-07-07 19:58:32 +0800

    增加分页

---
+++ source/_posts/SpringSecurity基础使用.md
@@ -0,0 +1,14 @@
+---
+layout: _posts
+title: SpringSecurity基础使用
+date: 2020-07-07 10:44:22
+tags:
+  - SpringSecurity
+  - SpringBoot
+categories:
+  - SpringSecurity
+---
+使用SpringSecurity认证、授权
+<!-- more -->
+## 1.前言
+## 2.依赖
--- themes/funnybug/source/js/page.js
+++ themes/funnybug/source/js/page.js
@@ -1,18 +1,3 @@
-layui.use('laypage', function(){
-	var laypage = layui.laypage;
+$(function() {
 
-	//分页
-	laypage.render({
-		elem: 'page'
-		,count: pageLength
-		,limit: 5
-	});
-	//上一页
-	$(document).on('click', '.layui-laypage-prev', function() {
-		alert("上一页");
-	});
-	//下一页
-	$(document).on('click', '.layui-laypage-next', function() {
-		alert("下一页");
-	});
-});
+})
