cf353cf59e762825dca7768c4643e4b522b9345c
tnaghan <user2@163.com>
2020-07-08 19:55:15 +0800

    增加页面底部

--- themes/funnybug/source/js/main.js
+++ themes/funnybug/source/js/main.js
@@ -1,28 +1,26 @@
 $(function() {
-	if ($(window).scrollTop() > 20) {
-          $("#backTop").show();
-        } else {
-          $("#backTop").hide();
-        }
+	// 页面刷新时，保留刷新前的滚动条位置
+	$("html,body").scrollTop(localStorage.reduction);
+	
     //当滚动条的位置处于距顶部20像素以下时，跳转链接出现，否则消失
-    $(function() {
-      $(window).scroll(function() {
+    $(window).scroll(function() {
+		localStorage.reduction = $(document).scrollTop();
+		$("#reduction").val();
         if ($(window).scrollTop() > 20) {
           $("#backTop").fadeIn(200);
         } else {
           $("#backTop").fadeOut(200);
         }
-      });
-      //当点击跳转链接后，回到页面顶部位置
-      $("#backTop").click(function() {
+    });
+    //当点击跳转链接后，回到页面顶部位置
+    $("#backTop").click(function() {
         $('body,html').animate({
           scrollTop: 0
         },
         250);
         return false;
-      });
     });
-	
+	// 文章单击事件
 	$("article").click(function() {
 		var path = $(this).attr("path");
 		window.location.href = path;
--- themes/funnybug/source/js/nav.js
+++ themes/funnybug/source/js/nav.js
@@ -1,9 +1,10 @@
-layui.use('element', function(){
-  var element = layui.element; //导航的hover效果、二级菜单等功能，需要依赖element模块
-  
-  //监听导航点击
-  element.on('nav(demo)', function(elem){
-    //console.log(elem)
-    layer.msg(elem.text());
-  });
+layui.use(['element', 'layer'], function(){
+	var element = layui.element; //导航的hover效果、二级菜单等功能，需要依赖element模块
+	var layer = layui.layer;
+	
+	//监听导航点击
+	element.on('nav(demo)', function(elem){
+		//console.log(elem)
+		layer.msg(elem.text());
+	});
 });
